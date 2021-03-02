4782b933134d740a6af2f5a2491859e298bb1c31
Дмитрий Подольский <podolsky@techart.ru>
2021-03-02 14:14:59 +0300

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      {{ some }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data: function () {
+      return {
+          some: null,
+      }
+    },
+  name: 'App',
+    created() {
+      axios.get('/api/some')
+        .then(res => this.some = res.data)
+    }
+}
+</script>
+
+<style>
+</style>
